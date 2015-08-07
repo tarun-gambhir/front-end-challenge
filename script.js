14bbfa8b2ec5b22227827e449c1391b8e4948e25
Tarun Gambhir <gambhir.tarun@gmail.com>
2015-08-07 13:34:01 -0700

    Commit - 1
    The working app

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var getPdts = $.Deferred();
+var domUpdated = $.Deferred();
+
+//show the spinner until page loads.
+$(window).bind("load", function () {
+	$('#inProgress').fadeOut(2000);
+});
 
 function domobj(){
   var self        =this;
@@ -8,7 +15,12 @@ function domobj(){
         for(i=0; i<response.sales.length ; i++){
           self.products.push( new productobj(response.sales[i], i)  );
         }
-    });
+		for( i=0; i< self.products.length ; i++){
+		  self.products[i].updatehtml();
+		}
+    }).done(function(){
+		getPdts.resolve();	//getProducts is complete, now we can update DOM.
+	});
   }
     
   self.updateproducthtml = function(){
@@ -20,14 +32,17 @@ function domobj(){
   self.updatedom = function(){
     var i=0
     thishtml='';
+	var pdts = [];
+		
     for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
+      if (i % 3 == 0 ){  thishtml += "<div class='row'>";}
       thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
+      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";}
+	 pdts[i] = self.products[i];
     }
-    $("#content").append(thishtml)
+    $("#content").append(thishtml);
+	domUpdated.resolve([pdts]);		//pass in the products array to doneCallback.
   }
-  
 }
 
 function productobj(product, i){
@@ -36,19 +51,54 @@ function productobj(product, i){
   self.title        = product.name
   self.tagline      = product.tagline
   self.url          = product.url
+  self.description	= product.description;
   self.htmlview     = ""
   self.index        = i
-  self.custom_class = "col"+ ((i % 3) +1)
-  
-  self.updatehtml= function(){
+
+   self.updatehtml= function(){
     $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
+      self.htmlview = template
+	  .replace('{image}', self.photo)
+	  .replace(/{title}/g, self.title)
+	  .replace('{tagline}', self.tagline)
+	  .replace('{url}', self.url);
     });
+
   }
 }
 
-
 var page=new domobj();
 page.getproducts('data.json');
-setTimeout("console.log('building html');page.updateproducthtml();",20);
-setTimeout("page.updatedom()",50)
+
+$.when(getPdts).then(function(){
+	setTimeout("page.updatedom()",500);
+});
+
+domUpdated.done(function(resp){
+    $(".product-container").hover(function(){
+			for(var i=0; i<resp[0].length; i++){
+				if($(this).data('title') == resp[0][i].title){
+					$(this).find(".product-box-overlay").html(resp[0][i].description);
+					$(this).find(".product-box-overlay").show();
+				}
+			}
+		},
+		function(){
+			$(this).find(".product-box-overlay").hide();
+		}
+	);
+	
+	$(".closeBtn").click(function(){
+		var pdt = $(this).parent().prev();
+		var self = $(this);
+		pdt.animate({
+			opacity: 0.25,
+			height: "toggle"
+		}, "slow", function() {
+			// Animation complete.
+			self.remove();
+		});
+		
+	});
+
+});
